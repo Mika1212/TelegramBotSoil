24ed42755f55a549c5b278cf9ba68ba7fd2505ef
polina <kierenc@yandex.ru>
2023-12-01 21:13:18 +0300

    fixed buttons' text and messages

--- src/config.py
+++ src/config.py
@@ -1,1 +1,5 @@
-BOT_TOKEN ='6919330886:AAEVpiswCzxhUejDpqk6CPkIhh_YdiyXmC8'
+from aiogram import Bot
+from aiogram.enums.parse_mode import ParseMode
+
+BOT_TOKEN = '6919330886:AAEVpiswCzxhUejDpqk6CPkIhh_YdiyXmC8'
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
--- src/kb.py
+++ src/kb.py
@@ -3,9 +3,9 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, \
 
 menu = [
     [InlineKeyboardButton(text="📸 Отправить фото", callback_data="send_photo"),
-     InlineKeyboardButton(text="❓ Как это работает", callback_data="how_it_works")],
+     InlineKeyboardButton(text="🔎 Инструкция", callback_data="instruction")],
     [InlineKeyboardButton(text="📝 Связаться с нами", callback_data="message_us"),
-     InlineKeyboardButton(text="🔎 Помощь", callback_data="help")]
+     InlineKeyboardButton(text="❓ Как это работает", callback_data="how_it_works")]
 ]
 menu = InlineKeyboardMarkup(inline_keyboard=menu)
 exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
